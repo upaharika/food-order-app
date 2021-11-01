52bcedf0f1a432af65209f7bf94fcca09f0a0851
upaharika <upaharikagawali@gmail.com>
2021-11-02 02:24:39 +0530

    food-order-app

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Delicious Meals</h1>
+        <HeaderCartButton onClick = {props.onShowCart}/>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious dishes!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
